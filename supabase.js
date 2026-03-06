32d899b4bf556895c865b0a7bff55a2d1114b0ce
Promise-Till-Death <experimentalsanjay@gmail.com>
2026-03-07 00:13:25 +0530

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,9 @@
+
+const SUPABASE_URL = "https://eodpcmgvbawutpcptaqz.supabase.co.";
+const SUPABASE_KEY = "sb_publishable_yvp-wrCv3PZhx_WASyM1Ng_5TlxpdZU.";
+
+const client = supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
+
